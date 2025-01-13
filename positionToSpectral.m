a5365a753e5402c2155c895563ed5dd0056f32f4
Articus <mirko.articus@zilxap12>
2025-01-13 19:28:18 +0100

    wvtl width calculation

--- positionToSpectral.m
+++ positionToSpectral.m
@@ -3,12 +3,17 @@ function cwt_behavior = positionToSpectral(xy_behavior,frameRate)
     tbw = 10;
     vpo = 10;
     FreqLim = [.5 15];
+    max_50wvlt_width = .6; 
     
     [~,Freqs,~,fb] = cwt(ones(size(xy_behavior,2),1),frameRate,'VoicesPerOctave',vpo,...
         'TimeBandwidth',tbw,'FrequencyLimits',FreqLim);
-    cwt_behavior = NaN(size(xy_behavior,1),numel(Freqs),size(xy_behavior,2));
+    wvlts = wavelets(fb);
+    wvlt_widths = getWaveletWidth(abs(wvlts),frameRate);
+    freq_slct = wvlt_widths/2<=max_50wvlt_width;
+    cwt_behavior = NaN(size(xy_behavior,1),sum(freq_slct),size(xy_behavior,2));
     for xyx = 1:size(xy_behavior,1)
-        cwt_behavior(xyx,:,:) = abs(cwt(xy_behavior(xyx,:),'Filterbank',fb));
+        tmp = abs(cwt(xy_behavior(xyx,:),'Filterbank',fb));
+        cwt_behavior(xyx,:,:) = tmp(freq_slct,:);
     end
 
 end
